6d625e50344e0a225ee3ef34d57af7c6cefc17ed
git config --global user.email joaopr0701@gmail.com <joaopr0701@gmail.com>
2024-10-17 23:54:26 -0300

    alterei o menu, agora falta criar as paginas e direcionar as  rotas

--- src/components/userMenu/userMenu.tsx
+++ src/components/userMenu/userMenu.tsx
@@ -1,19 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useAuth } from '../../main';
 import { LogoutButton } from '../btnLogout/btnLogout';
 import { Link } from 'react-router-dom';
-import './userMenu.css'; // Importar o arquivo de estilos
+import './userMenu.css';
 
 export const UserMenu: React.FC = () => {
   const { currentUser } = useAuth();
   const [menuOpen, setMenuOpen] = useState(false);
+  const menuRef = useRef<HTMLDivElement>(null);
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
 
+  const handleClickOutside = (event: MouseEvent) => {
+    if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+      setMenuOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
   return (
-    <div className="user-menu-container">
+    <div className="user-menu-container" ref={menuRef}>
       {currentUser ? (
         <>
           <div className="user-info" onClick={toggleMenu}>
@@ -22,7 +36,6 @@ export const UserMenu: React.FC = () => {
               className="user-avatar"
               alt="User Avatar"
             />
-       
             <span className="dropdown-arrow">&#9662;</span>
           </div>
           {menuOpen && (
@@ -31,13 +44,10 @@ export const UserMenu: React.FC = () => {
                 <span className="user-name">{currentUser.displayName}</span>
                 <span className="user-email">{currentUser.email}</span>
               </div>
-
               <ul className="menu-items">
-                <li><Link to="/perfil"> Perfil </Link></li>
+                <li><Link to="/perfil">Perfil</Link></li>
                 <li><Link to="/settings">Configurações</Link></li>
-
-                <div className="lgt"> <li><LogoutButton /></li>  </div>
-               
+                <li><LogoutButton /></li>
               </ul>
             </div>
           )}
