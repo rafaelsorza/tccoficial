c87772a953d13022b970b43ad4e41d193857e579
Wilson Leonardo <wilsonleonardo@MacBook-Air-de-Wilson.local>
2024-12-04 19:57:51 -0300

    codigo q vc mandou

--- src/components/NutritionChart/NutritionChart.tsx
+++ src/components/NutritionChart/NutritionChart.tsx
@@ -1,42 +1,53 @@
 import React from 'react';
-import { Chart, ArcElement, Tooltip, Legend } from 'chart.js';
-import { Doughnut } from 'react-chartjs-2';
+import { PieChart, Pie, Tooltip, Cell } from 'recharts';
 
-Chart.register(ArcElement, Tooltip, Legend);
-
-interface NutritionalValues {
-  totalCalories: number;
-  totalProtein: number;
-  totalVitamins: number;
-  totalCarbohydrates: number;
+interface ChartData {
+  name: string;
+  value: number;
 }
 
-interface NutritionalChartProps {
-  nutritionalValues: NutritionalValues;
+interface CaloriasChartProps {
+  data: ChartData[];
 }
 
-const NutritionalChart: React.FC<NutritionalChartProps> = ({ nutritionalValues }) => {
-  const data = {
-    labels: [ 'Proteínas', 'Vitaminas', 'Carboidratos'],
-    datasets: [
-      {
-        data: [
-          nutritionalValues.totalProtein,
-          nutritionalValues.totalVitamins,
-          nutritionalValues.totalCarbohydrates
-        ],
-        backgroundColor: ['#72BBFF', '#FEF074', '#FCA1FE'],
-        borderColor: '#fff',
-        borderWidth: 1,
-      }
-    ]
-  };
+const CaloriasChart: React.FC<CaloriasChartProps> = ({ data }) => {
+  const COLORS = ['#0088FE', '#00C49F', '#FFBB28'];
 
   return (
-    <div className="pizza-chart">
-      <Doughnut data={data} />
+    <div className="chart-container">
+      <h2>Nutrição do Dia</h2>
+      <PieChart width={600} height={300}>
+        <Pie
+          data={data}
+          dataKey="value"
+          nameKey="name"
+          cx="50%"
+          cy="50%"
+          outerRadius={100}
+          innerRadius={50}
+          label
+        >
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
+        <Tooltip />
+      </PieChart>
+      <div className="chart-legend">
+        {data.map((entry, index) => (
+          <div key={index} className="legend-item">
+            <div
+              className="color-box"
+              style={{ backgroundColor: COLORS[index % COLORS.length] }}
+            ></div>
+            <span>
+              {entry.name}: {entry.value.toFixed(2)}
+            </span>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
 
-export default NutritionalChart;
+export default CaloriasChart;
